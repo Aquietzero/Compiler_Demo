7a1ee20e018dac2205476db4252efc26e0f2b6f6
Yunhao Zhao <zhaoyunhaosss@gmail.com>
2011-11-27 13:46:31 +0800

    reconstruct file structure

---
+++ ui/inputPage.js
@@ -0,0 +1,18 @@
+var DEFAULT_TERMINALS = "id, +, *, (, )",
+    DEFAULT_GRAMMAR   = "E -> E + T | T\nT -> T * F | F\nF -> ( E ) | id";
+
+
+$(function() {
+    document.getElementById("terminalsInput").value = DEFAULT_TERMINALS;
+    document.getElementById("grammarInput").value = DEFAULT_GRAMMAR;
+
+    $("#grammarConfirm").bind("click", showConfirmPage);
+});
+
+function showInputPage() {
+    hideNavigation();
+    hideAllPages();
+
+    $("#inputPage").fadeIn("slow");
+    $("#introductionPart").fadeIn("slow");
+}
